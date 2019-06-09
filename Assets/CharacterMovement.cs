207fcaa4bd32a65d5dc30ebe5277341724339c19
nnlquan <longquan0104@gmail.com>
2019-06-10 01:29:57 +0700

    Initial commit

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float JumpSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-speed, 0));
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Animator>().SetTrigger("Run");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(speed, 0));
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetTrigger("Run");
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpSpeed));
+        }
+    }
+}
